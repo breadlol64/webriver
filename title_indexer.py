5b009c4fdbb8237d94576726609c69716011aab0
artur <breadlol64@gmail.com>
2023-08-16 11:12:02 +0300

    Add title indexer

---
+++ title_indexer.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+indexed_titles = {}
+
+with open("test_links.txt", 'r') as indexed_links_f:
+    for link in indexed_links_f.readlines():
+        link = link.strip()
+        content = requests.get(link).content
+        bs = BeautifulSoup(content, "html.parser")
+        title = bs.title.string
+        indexed_titles[link] = title
+
+with open("indexed_titles.json", "w") as indexed_titles_f:
+    json.dump(indexed_titles, indexed_titles_f)
